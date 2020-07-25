d51124f7787ecbf286e230dd8daceceaed187cff
nganluu <nganltt712@gmail.com>
2020-07-25 10:06:54 +0700

    add Signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Signin from './components/SignIn';
+import Signup from './components/Signup';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/signin" component = {Signin}/>
+            <Route exact path="/signup" component = {Signup}/>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
