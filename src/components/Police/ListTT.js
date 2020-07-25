ee17e914e913c4d3daa907244b32cc434fcbcb94
nganluu <nganltt712@gmail.com>
2020-07-26 02:07:06 +0700

    add dktt

--- src/components/Police/ListTT.js
+++ src/components/Police/ListTT.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import Header from './Header'
+import cccd from '../../assets/image/cccd.png';
+import hk from '../../assets/image/hk.jpg'
 import '../../assets/css/Temporary.css'
-import { Table, Button, Modal, ModalBody, ModalHeader, ModalFooter } from 'reactstrap';
+import tick from '../../assets/image/tick.png'
+import { Spinner, Table, Button, Modal, ModalBody, ModalHeader, ModalFooter } from 'reactstrap';
 
 const list = [{
+    "id": "1",
     "name": "Mark",
     "CMT": "112233445566"
 },
 {
+    "id": "2",
     "name": "Jacob",
     "CMT": "123456789121"
 }, 
 {
+    "id": "3",
     "name": "Nguyen Thi Mung",
     "CMT": "161022012341"
 }
@@ -21,9 +27,13 @@ export default class ListTT extends Component {
         super(props);
         this.state={
             modal: false,
-            del: false
+            del: false,
+            xn : false, 
+            count: 0,
         }
     }
+   
+    
     handleCheck = (id)=>{
         console.log(id)
         this.setState({modal: !this.state.modal})
@@ -38,7 +48,20 @@ export default class ListTT extends Component {
         this.setState({del: !this.state.del})
     }
     handleDel = (id) => {
-
+        this.setState({del: !this.state.del})
+    }
+    handleXN = ()=>{
+        this.setState({xn: !this.state.xn})
+    }
+    handleXNout = () => {
+        this.setState({
+            xn: !this.state.xn,
+            modal: !this.state.modal })
+    }
+    increase = ()=>{
+        this.setState({
+            count: this.state.count + 1
+        })
     }
     render() {
         const count = 0;
@@ -61,15 +84,16 @@ export default class ListTT extends Component {
       <tbody>
        {list.map((item)=>
         <tr id={item.CMT}>
-          <th scope="row">{count+1}</th>
+          <th scope="row">{item.id}</th>
           <td>{item.name}</td>
           <td>{item.CMT}</td>
           <td className="row" style={{ display: "inline-block"}}>
-                <Button id={item.CMT} onClick={()=>this.handleCheck(item.CMT)} color="success" style={{marginRight: "1rem  "}}>Kiểm tra</Button> 
+                <Button id={item.CMT} onClick={()=>this.handleCheck(item.CMT)} color="success" 
+                    style={{marginRight: "1rem"}}>Kiểm tra</Button> 
                 <Button onClick={()=>this.onDelete(item.CMT)} color="danger">Hủy</Button>
           </td>
         </tr>
-        
+       
        )} 
        <Modal isOpen={this.state.del} toggle={this.onDelete} >
         <ModalHeader >Xác nhận hủy đơn</ModalHeader>
@@ -94,8 +118,10 @@ export default class ListTT extends Component {
                     <div className="content-form">
                         <div className="row">
                             <div className="fild usename">
-                                <label>Nhập họ và tên:</label>
+                                <label>Họ và tên:</label>
                                 <input
+                                    disabled
+                                    placeholder="Nguyen Thi Mung"
                                     type='text'
                                     name='username'
                                 />
@@ -103,6 +129,8 @@ export default class ListTT extends Component {
                             <div className="fild genre">
                                 <label>Giới tính</label>
                                 <input
+                                disabled
+                                placeholder="Nu"
                                     type='text'
                                     name='genre'
                                 />
@@ -110,6 +138,8 @@ export default class ListTT extends Component {
                             <div className="fild national">
                                 <label>Quốc tịch</label>
                                 <input
+                                disabled
+                                placeholder="Viet Nam"
                                     type='text'
                                     name='national'
                                 />
@@ -117,38 +147,38 @@ export default class ListTT extends Component {
                         </div>
                         <div className="row">
                             <div className="fild cmt">
-                                <label>Chứng minh thư</label>
+                                <label>CMT/CCCD</label>
                                 <input
+                                disabled 
+                                placeholder="162265666651"
                                     type='text'
                                     name='cmt'
                                 />
                             </div>
                             <div className="fild DCTT">
                                 <label>Địa chỉ thường trú</label>
-                                <input
-                                    type='text'
-                                    name='DCTT'
-                                />
+                                <br/>
+                                <text>So 12, pho Bach Mai, quan Hai Ba Trung, Ha Noi</text>
+                              
+                                
                             </div>
                         </div>
                         <div className="row">
-                            <div className=" fild BKNK">
-                                <label>
-                                    Ảnh chụp chứng minh thư
-                                </label>
-                                <input type="file" name="BKNK"/>
-                            </div>
+                        <div className="fild BKNK">
+                                 <label>
+                                    Ảnh chụp CMT/CCCD
+                                </label><br/>
+                           <img alt="" src={cccd} style={{width: "100px"}}/>
+                           </div>
                         </div>
                         <div className="row">
                             <div className=" fild BKNK">
                                 <label>
                                     Ảnh chụp các giấy tờ khác cần xác thực
-                                </label>
-                                <input type="file" name="BKNK"/>
+                                </label><br/>
+                                <img alt="" src={hk} style={{width: "100px"}}/>
                             </div>
                         </div>
-                      
-                        <input type="submit" value="Submit" className="submit"/>
                     </div>
                     
                 </form>
@@ -156,9 +186,23 @@ export default class ListTT extends Component {
                     </ModalBody>
                     <ModalFooter>
                     <Button type="submit" color="secondary" onClick={this.handleCancel}>Cancel</Button>
-                    <Button type="submit" color="success" onClick={this.fetchSubmit}>Xác nhận</Button>
+                    <Button type="submit" color="success" onClick={this.handleXN}>Xác nhận</Button>
                     </ModalFooter>
                 </Modal>
+                <Modal isOpen={this.state.xn} toggle={this.handleXN} >
+        <ModalHeader >Xác nhận thanh công</ModalHeader>
+        <ModalBody style={{textAlign: "center"}}>
+            <img src = {tick} alt="" style={{width: "20%", color: "green", margin: "1rem"}}/>
+            {/* <Spinner size="sm" color="primary" /> */}
+            <br/>
+            <text>Xác thực công dân hợp lệ</text>
+        </ModalBody>
+        <ModalFooter>
+        
+        <Button color="secondary" onClick={this.handleXN} >Cancel</Button>
+          <Button color="success" onClick={this.handleXNout}>OK</Button>{' '}
+        </ModalFooter>
+      </Modal>
             </div>
         )
     }
