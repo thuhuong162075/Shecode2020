40261e0dc9e06d376e884de4483f948f2f8d1765
Thu Huong <huong.ttt171298@gmail.com>
2020-07-26 08:03:40 +0700

    create callapi

--- src/components/Temporary.js
+++ src/components/Temporary.js
@@ -5,10 +5,30 @@ class Temporary extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-        username: '',
-        age: null,
+            username: '',
+            genre: '',
+            national: '',
+            cmt: '',
+            DCTT: {
+                    city: '',
+                    district: '',
+                    village: '',
+                    phuong: ''
+                },
+            imgCMT: {},
+            imgPage: {}
         };
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.name === 'isGoing' ? target.checked : target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+      }
     render() {
         return (
             <div className="temporary">
@@ -21,6 +41,8 @@ class Temporary extends React.Component {
                                 <input
                                     type='text'
                                     name='username'
+                                    value={this.state.username}
+                                    onChange={this.handleInputChange}
                                 />
                             </div>
                             <div className="fild genre">
@@ -28,6 +50,8 @@ class Temporary extends React.Component {
                                 <input
                                     type='text'
                                     name='genre'
+                                    value={this.state.genre}
+                                    onChange={this.handleInputChange}
                                 />
                             </div>
                             <div className="fild national">
@@ -35,6 +59,8 @@ class Temporary extends React.Component {
                                 <input
                                     type='text'
                                     name='national'
+                                    value={this.state.national}
+                                    onChange={this.handleInputChange}
                                 />
                             </div>
                         </div>
@@ -44,30 +70,61 @@ class Temporary extends React.Component {
                                 <input
                                     type='text'
                                     name='cmt'
+                                    value={this.state.cmt}
+                                    onChange={this.handleInputChange}
                                 />
                             </div>
                             <div className="fild DCTT">
-                                <label>Địa chỉ thường trú</label>
-                                <input
-                                    type='text'
-                                    name='DCTT'
-                                />
+                                <label>Địa chỉ thường trú: </label>
+                                <div className="DetailDCTT">
+                                    <div className="Detail city">
+                                        <label>Tỉnh</label>
+                                        <select>
+                                            <option value="HaiDuong">Hải Dương</option>
+                                            <option value="HaNoi" selected>Hà Nội</option>
+                                            <option value="HaNam">Hà Nam</option>
+                                        </select>
+                                    </div>
+                                    <div className="Detail district">
+                                        <label>Quận/Huyện</label>
+                                        <select>
+                                            <option value="HoanKiem">Hoàn Kiếm</option>
+                                            <option value="HaiBaTrung" selected>Hai Bà Trưng</option>
+                                            <option value="ThanhXuan">Thanh Xuân</option>
+                                        </select>
+                                    </div> 
+                                    <div className="Detail phuong">
+                                        <label>Phường/Xã</label>
+                                        <select>
+                                            <option value="BachKhoa">Bách Khoa</option>
+                                            <option value="TranDaiNghia" selected>Trần Đại Nghĩa</option>
+                                        </select>
+                                    </div> 
+                                    <div className="Detail village">
+                                        <label>Số nhà/làng</label>
+                                        <select>
+                                            <option value="BachKhoa">21</option>
+                                            <option value="TranDaiNghia" selected>30</option>
+                                        </select>
+                                    </div> 
+                                </div>
+                                    
                             </div>
                         </div>
                         <div className="row">
-                            <div className=" fild BKNK">
+                            <div className=" fild imgCMT">
                                 <label>
-                                    Ảnh chụp chứng minh thư
+                                    Ảnh chụp chứng minh thư (Chụp 2 mặt)
                                 </label>
-                                <input type="file" name="BKNK"/>
+                                <input type="file" name="imgCMT"/>
                             </div>
                         </div>
                         <div className="row">
-                            <div className=" fild BKNK">
+                            <div className=" fild imgPage">
                                 <label>
-                                    Ảnh chụp các giấy tờ khác cần xác thực
+                                    Ảnh chụp các giấy tờ khác cần xác thực khác
                                 </label>
-                                <input type="file" name="BKNK"/>
+                                <input type="file" name="imgPage"/>
                             </div>
                         </div>
                       
